e40b22b0ca4b888974b222dd83ec7b757f6fe5d2
shenlin <141220087@smail.nju.edu.cn>
2017-05-06 13:28:24 +0800

    Add files via upload
    登录 主页

---
+++ www/js/login.js
@@ -0,0 +1,10 @@
+function loginSubmit(id1,password1)
+{
+	//alert(name1+" "+content1+" "+dept);
+	var user = new Object(); //创建对象 
+	//alert(id1+"\n"+password1);
+	user.userPhone=id1;
+	user.userPwd=password1;
+	alert(user.userPhone+"\n"+userPwd);
+	return user;
+}
